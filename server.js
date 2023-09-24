0d52919881f24fde40afffa7f0953302481da9d9
Fuad Maulana <fuadm03@gmail.com>
2023-09-25 03:06:04 +0700

    feat: set ejs view engine

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const mongoose = require('mongoose')
 const ShortUniqueId = require('short-unique-id')
 
 const app = express()
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false}) )
 
 app.get('/', (req, res) => {
-  res.send(`hello`)
+  res.render('index')
 })
 
 app.listen(3000, () => console.log('server started'))
